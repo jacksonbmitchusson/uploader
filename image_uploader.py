7c2b017154da14171e52bed8dc32417920466aa0
jacksonbmitchusson <mr080guy@gmail.com>
2025-10-24 02:29:39 -0500

    odiu wadaw oihhdaw o

--- image_uploader.py
+++ image_uploader.py
@@ -8,7 +8,7 @@ base_path = '/home/onaquest/server-output'
 def get_list():
     lists = [
         [
-            [file, os.path.getsize(f'{base_path}/{file}')] 
+            [file, os.path.getsize(f'{base_path}/images/{id}/{file}')] 
             for file in os.listdir(f'{base_path}/images{id}')] 
         for id in range(2)]
 
